6671d9d3cb5b7f1ce7af8b021d82863302f6f8e7
Sweepyoface <sweepy@sweepy.pw>
2019-03-30 19:44:24 -0400

    Remove unnecessary test & refactor

--- src/test/index.test.ts
+++ src/test/index.test.ts
@@ -1,15 +1,15 @@
 import chalk from "chalk"
 
-import { Logger as logger } from "./index"
-import LogLevel from "./lib/LogLevel"
-import { LogScope } from "./lib/LogScope"
+import { Logger as logger } from "../index"
+import LogLevel from "../lib/LogLevel"
+import { LogScope } from "../lib/LogScope"
 
 const testOutput = "Test output"
 const level = logger.getLogLevelByName("warn")
 
 logger.setLogLevel(level) // don't use default
 
-test("test log formatting", () => {
+test("test formatting", () => {
     logger.setPrintTimestamps(false)
     const result = logger.format(level, testOutput)
     expect(result).toBe(`\u001b[33m\u001b[1mWARN\u001b[22m\u001b[39m ${testOutput}`)
@@ -21,16 +21,12 @@ test("test json formatting", () => {
     expect(result).toMatch(new RegExp(`^{"time":".*","level":${JSON.stringify(level)},"message":"${testOutput}"}$`))
 })
 
-test("test printing", async () => {
-    expect(typeof await logger.warn(testOutput)).toBe("string")
-})
-
-test("test higher log level", async () => {
-    expect(await logger.info(testOutput)).toBeUndefined()
-})
-
 test("test customization", () => {
     logger.setPrintTimestamps(false)
     const result = logger.log(new LogLevel("fancy", chalk.magenta.underline, -1, LogScope.STDOUT), testOutput)
     expect(result).toBe(`${chalk.magenta.underline("FANCY")} ${testOutput}`)
+})
+
+test("test higher log level", async () => {
+    expect(await logger.info(testOutput)).toBeUndefined()
 })
